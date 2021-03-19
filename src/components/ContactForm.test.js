cd0ec7a4dfdbbc43085e4d549527d5536ba90932
unknown <calanhaga@aim.com>
2021-03-18 21:28:11 -0400

    renders the contact form header passed

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -10,7 +10,13 @@ test("renders without errors", () => {
   expect(errors.length).toEqual(0);
 });
 
-test("renders the contact form header", () => {});
+test("renders the contact form header", () => {
+  render(<ContactForm />);
+  const header = screen.queryByText(/contact form/i);
+  expect(header).toBeInTheDocument();
+  expect(header).toBeTruthy();
+  expect(header).toHaveTextContent("Contact Form");
+});
 
 test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {});
 
